baea4837ef51b3f82dae5ff2da4cd4b2c6a15f5d
Salih Dilmen <80348831+Salhdlmn@users.noreply.github.com>
2025-04-14 15:45:53 +0300

    first commit

---
+++ MyPortfolioUdemy/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortfolioUdemy.DAL.Entities;
+
+namespace MyPortfolioUdemy.DAL.Context
+{
+    public class MyPortfolioContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-M9MA5UL; initial Catalog=MyPortfolioDb;" +
+                "integrated security=true; trust server certificate = true;");// Baglantı stillerini vericek olan method.
+        }
+
+        public DbSet<About>Abouts { get; set; }
+        public DbSet<Contact>Contacts { get; set; }
+        public DbSet<Experience>Experiences { get; set; }
+        public DbSet<Feature>Features { get; set; }
+        public DbSet<Message>Messages { get; set; }
+        public DbSet<Portfolio>Portfolios { get; set; }
+        public DbSet<Skill>Skills { get; set; }
+        public DbSet<SocialMedia>SocialMedias { get; set; }
+        public DbSet<Testimonial>Testimonials { get; set; }
+        public DbSet<ToDoList>ToDoLists { get; set; }
+    }
+}
---
+++ MyPortfolioUdemy/DAL/Entities/Portfolio.cs
@@ -0,0 +1,17 @@
+﻿namespace MyPortfolioUdemy.DAL.Entities
+{
+    public class Portfolio
+    {
+        public int PortfolioId { get; set; }
+
+        public string Title { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string ImageURl { get; set; }
+
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+    }
+}
